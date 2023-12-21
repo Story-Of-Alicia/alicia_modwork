233755602e5197f4ee95c8793148d29903403c0a
rgnter <maros.prejsa1@gmail.com>
2023-12-21 20:29:11 +0100

    more documentation

--- alicia_modder/source/main/mod/alicia/gametree/GTLx.h
+++ alicia_modder/source/main/mod/alicia/gametree/GTLx.h
@@ -33,82 +33,84 @@ typedef struct GTL GTL;
 extern "C" {
 #endif 
 
-#ifndef __GTLxLib_LIBRARY_DEFINED__
-#define __GTLxLib_LIBRARY_DEFINED__
+#ifndef GTLxLib_LIBRARY_DEFINED
+#define GTLxLib_LIBRARY_DEFINED
 
+//! IID of LIBID_GTLxLib
 EXTERN_C const IID LIBID_GTLxLib;
 
-#ifndef ___IGTLEvents_DISPINTERFACE_DEFINED__
-#define ___IGTLEvents_DISPINTERFACE_DEFINED__
+#ifndef IGTLEvents_DISPINTERFACE_DEFINED
+#define IGTLEvents_DISPINTERFACE_DEFINED
 
-
+//! IID of DIID__IGTLEvents
 EXTERN_C const IID DIID__IGTLEvents;
 
 #if defined(__cplusplus) && !defined(CINTERFACE)
 
-    MIDL_INTERFACE("F943E25C-00F9-408B-9DAA-F4A3EFA9A552")
-    _IGTLEvents : public IDispatch
-    {
-    };
+//! RPC MIDL Interface for GTL Events
+MIDL_INTERFACE("F943E25C-00F9-408B-9DAA-F4A3EFA9A552")
+_IGTLEvents : public IDispatch
+{
+};
     
 #else 
 
-    typedef struct _IGTLEventsVtbl
-    {
-        BEGIN_INTERFACE
-        
-        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( _IGTLEvents * This,REFIID riid,_COM_Outptr_  void **ppvObject);
-        
-        ULONG ( STDMETHODCALLTYPE *AddRef )( 
-            _IGTLEvents * This);
-        
-        ULONG ( STDMETHODCALLTYPE *Release )( 
-            _IGTLEvents * This);
-        
-        HRESULT ( STDMETHODCALLTYPE *GetTypeInfoCount )( 
-            _IGTLEvents * This,
-             UINT *pctinfo);
-        
-        HRESULT ( STDMETHODCALLTYPE *GetTypeInfo )( 
-            _IGTLEvents * This,
-             UINT iTInfo,
-             LCID lcid,
-             ITypeInfo **ppTInfo);
-        
-        HRESULT ( STDMETHODCALLTYPE *GetIDsOfNames )( 
-            _IGTLEvents * This,
-            REFIID riid,
-             LPOLESTR *rgszNames,
-             UINT cNames,
-             LCID lcid,
-             DISPID *rgDispId);
-        
-        HRESULT ( STDMETHODCALLTYPE *Invoke )( 
-            _IGTLEvents * This,
-             
-            _In_  DI 
-            _In_  REFIID riid,
-    
-            _In_  LCID lcid,
-             
-            _In_  WORD wFlags,
-             
-            _In_  DISPPARAMS *pDispParams,
-           
-            _Out_opt_  VARIANT *pVarResult,
-            
-            _Out_opt_  EXCEPINFO *pExcepInfo,
-             
-            _Out_opt_  UINT *puArgErr);
-        
-        END_INTERFACE
-    } _IGTLEventsVtbl;
+//! Direct ABI interface for GTL Events
+typedef struct _IGTLEventsVtbl
+{
+  BEGIN_INTERFACE
 
-    interface _IGTLEvents
-    {
-        CONST_VTBL struct _IGTLEventsVtbl *lpVtbl;
-    };
+  HRESULT ( STDMETHODCALLTYPE *QueryInterface )( _IGTLEvents * This,REFIID riid,_COM_Outptr_  void **ppvObject);
 
+  ULONG ( STDMETHODCALLTYPE *AddRef )(
+      _IGTLEvents * This);
+
+  ULONG ( STDMETHODCALLTYPE *Release )(
+      _IGTLEvents * This);
+
+  HRESULT ( STDMETHODCALLTYPE *GetTypeInfoCount )(
+      _IGTLEvents * This,
+       UINT *pctinfo);
+
+  HRESULT ( STDMETHODCALLTYPE *GetTypeInfo )(
+      _IGTLEvents * This,
+       UINT iTInfo,
+       LCID lcid,
+       ITypeInfo **ppTInfo);
+
+  HRESULT ( STDMETHODCALLTYPE *GetIDsOfNames )(
+      _IGTLEvents * This,
+      REFIID riid,
+       LPOLESTR *rgszNames,
+       UINT cNames,
+       LCID lcid,
+       DISPID *rgDispId);
+
+  HRESULT ( STDMETHODCALLTYPE *Invoke )(
+      _IGTLEvents * This,
+
+      _In_  DI
+      _In_  REFIID riid,
+
+      _In_  LCID lcid,
+
+      _In_  WORD wFlags,
+
+      _In_  DISPPARAMS *pDispParams,
+
+      _Out_opt_  VARIANT *pVarResult,
+
+      _Out_opt_  EXCEPINFO *pExcepInfo,
+
+      _Out_opt_  UINT *puArgErr);
+
+  END_INTERFACE
+} _IGTLEventsVtbl;
+
+interface _IGTLEvents
+{
+    CONST_VTBL struct _IGTLEventsVtbl *lpVtbl;
+};
 
 #ifdef COBJMACROS
 
@@ -135,124 +137,127 @@ EXTERN_C const IID DIID__IGTLEvents;
     ( (This)->lpVtbl -> Invoke(This,dispIdMember,riid,lcid,wFlags,pDispParams,pVarResult,pExcepInfo,puArgErr) ) 
 
 #endif // COBJMACROS
-#endif
-#endif
+#endif // __cplusplus and not cinterface
+#endif // IGTLEvents_DISPINTERFACE_DEFINED
 
-#ifndef __IGTL_INTERFACE_DEFINED__
-#define __IGTL_INTERFACE_DEFINED__
+#ifndef IGTL_INTERFACE_DEFINED
+#define IGTL_INTERFACE_DEFINED
 
+//! IID of GTL
 EXTERN_C const IID IID_IGTL;
 
 #if defined(__cplusplus) && !defined(CINTERFACE)
-    
-    MIDL_INTERFACE("D32A8602-1F90-4453-A746-A81671F03FB5")
-    IGTL : public IDispatch
-    {
-    public:
-        virtual HRESULT STDMETHODCALLTYPE Run(SAFEARRAY * arguments) = 0;
-        
-        virtual HRESULT STDMETHODCALLTYPE RunExt(SAFEARRAY * arguments) = 0;
-        
-        virtual HRESULT STDMETHODCALLTYPE Runs_Variable(SAFEARRAY * arguments) = 0;
-        
-        virtual HRESULT STDMETHODCALLTYPE GetInfo(BSTR name, BSTR *info) = 0;
-        
-        virtual  HRESULT STDMETHODCALLTYPE GetSystemInfo( 
-             BSTR name,
-             BSTR *info) = 0;
-        
-        virtual  HRESULT STDMETHODCALLTYPE ProcessCheck( 
-             BSTR processName,
-             BSTR *result) = 0;
-        
-    };
+
+//! RPC MIDL Interface for GTL
+MIDL_INTERFACE("D32A8602-1F90-4453-A746-A81671F03FB5")
+IGTL : public IDispatch
+{
+public:
+  virtual HRESULT STDMETHODCALLTYPE Run(SAFEARRAY * arguments) = 0;
+
+  virtual HRESULT STDMETHODCALLTYPE RunExt(SAFEARRAY * arguments) = 0;
+
+  virtual HRESULT STDMETHODCALLTYPE Runs_Variable(SAFEARRAY * arguments) = 0;
+
+  virtual HRESULT STDMETHODCALLTYPE GetInfo(BSTR name, BSTR *info) = 0;
+
+  virtual  HRESULT STDMETHODCALLTYPE GetSystemInfo(
+       BSTR name,
+       BSTR *info) = 0;
+
+  virtual  HRESULT STDMETHODCALLTYPE ProcessCheck(
+       BSTR processName,
+       BSTR *result) = 0;
+
+};
 
 #else
 
-    typedef struct IGTLVtbl
-    {
-        BEGIN_INTERFACE
-        
-        HRESULT ( STDMETHODCALLTYPE *QueryInterface )( 
-            IGTL * This, REFIID riid, _COM_Outptr_  void **ppvObject);
-        
-        ULONG ( STDMETHODCALLTYPE *AddRef )( 
-            IGTL * This);
-        
-        ULONG ( STDMETHODCALLTYPE *Release )( 
-            IGTL * This);
-        
-        HRESULT ( STDMETHODCALLTYPE *GetTypeInfoCount )( 
-            IGTL * This,
-             UINT *pctinfo);
-        
-        HRESULT ( STDMETHODCALLTYPE *GetTypeInfo )( 
-            IGTL * This,
-             UINT iTInfo,
-             LCID lcid,
-             ITypeInfo **ppTInfo);
-        
-        HRESULT ( STDMETHODCALLTYPE *GetIDsOfNames )( 
-            IGTL * This,
-             REFIID riid,
-             LPOLESTR *rgszNames,
-            UINT cNames,
-             LCID lcid,
-            DISPID *rgDispId);
-        
-         HRESULT ( STDMETHODCALLTYPE *Invoke )( 
-            IGTL * This,
-            
-            _In_  DISPID dispIdMember,
-            
-            _In_  REFIID riid,
-             
-            _In_  LCID lcid,
-             
-            _In_  WORD wFlags,
-            
-            _In_  DISPPARAMS *pDispParams,
-            
-            _Out_opt_  VARIANT *pVarResult,
-            
-            _Out_opt_  EXCEPINFO *pExcepInfo,
-             
-            _Out_opt_  UINT *puArgErr);
-        
-         HRESULT ( STDMETHODCALLTYPE *Run )( 
-            IGTL * This,
-            SAFEARRAY * arguments);
-        
-         HRESULT ( STDMETHODCALLTYPE *RunExt )( 
-            IGTL * This,
-            SAFEARRAY * arguments);
-        
-         HRESULT ( STDMETHODCALLTYPE *Runs_Variable )( 
-            IGTL * This,
-            SAFEARRAY * arguments);
-        
-         HRESULT ( STDMETHODCALLTYPE *GetInfo )( 
-            IGTL * This,
-             BSTR name,
-             BSTR *info);
-        
-         HRESULT ( STDMETHODCALLTYPE *GetSystemInfo )( 
-            IGTL * This,
-             BSTR name,
-            BSTR *info);
-        
-        HRESULT ( STDMETHODCALLTYPE *ProcessCheck )( 
-            IGTL * This,
-             BSTR processName,
-             BSTR *result);
-        
-        END_INTERFACE
-    } IGTLVtbl;
+//! Direct ABI interface for GTL
+typedef struct IGTLVtbl
+{
+  BEGIN_INTERFACE
 
-    interface IGTL
-    {
-        CONST_VTBL struct IGTLVtbl *lpVtbl;
-    };
+  HRESULT ( STDMETHODCALLTYPE *QueryInterface )(
+      IGTL * This, REFIID riid, _COM_Outptr_  void **ppvObject);
+
+  ULONG ( STDMETHODCALLTYPE *AddRef )(
+      IGTL * This);
+
+  ULONG ( STDMETHODCALLTYPE *Release )(
+      IGTL * This);
+
+  HRESULT ( STDMETHODCALLTYPE *GetTypeInfoCount )(
+      IGTL * This,
+       UINT *pctinfo);
+
+  HRESULT ( STDMETHODCALLTYPE *GetTypeInfo )(
+      IGTL * This,
+       UINT iTInfo,
+       LCID lcid,
+       ITypeInfo **ppTInfo);
+
+  HRESULT ( STDMETHODCALLTYPE *GetIDsOfNames )(
+      IGTL * This,
+       REFIID riid,
+       LPOLESTR *rgszNames,
+      UINT cNames,
+       LCID lcid,
+      DISPID *rgDispId);
+
+   HRESULT ( STDMETHODCALLTYPE *Invoke )(
+      IGTL * This,
+
+      _In_  DISPID dispIdMember,
+
+      _In_  REFIID riid,
+
+      _In_  LCID lcid,
+
+      _In_  WORD wFlags,
+
+      _In_  DISPPARAMS *pDispParams,
+
+      _Out_opt_  VARIANT *pVarResult,
+
+      _Out_opt_  EXCEPINFO *pExcepInfo,
+
+      _Out_opt_  UINT *puArgErr);
+
+   HRESULT ( STDMETHODCALLTYPE *Run )(
+      IGTL * This,
+      SAFEARRAY * arguments);
+
+   HRESULT ( STDMETHODCALLTYPE *RunExt )(
+      IGTL * This,
+      SAFEARRAY * arguments);
+
+   HRESULT ( STDMETHODCALLTYPE *Runs_Variable )(
+      IGTL * This,
+      SAFEARRAY * arguments);
+
+   HRESULT ( STDMETHODCALLTYPE *GetInfo )(
+      IGTL * This,
+       BSTR name,
+       BSTR *info);
+
+   HRESULT ( STDMETHODCALLTYPE *GetSystemInfo )(
+      IGTL * This,
+       BSTR name,
+      BSTR *info);
+
+  HRESULT ( STDMETHODCALLTYPE *ProcessCheck )(
+      IGTL * This,
+       BSTR processName,
+       BSTR *result);
+
+  END_INTERFACE
+} IGTLVtbl;
+
+interface IGTL
+{
+  CONST_VTBL struct IGTLVtbl *lpVtbl;
+};
 
 #ifdef COBJMACROS
 
@@ -307,7 +312,7 @@ EXTERN_C const CLSID CLSID_GTL;
 class DECLSPEC_UUID("99846979-30FD-453E-A427-99217EA62571")
 GTL;
 #endif
-#endif 
+#endif // GTLxLib_LIBRARY_DEFINED
 
 #ifdef __cplusplus
 }
